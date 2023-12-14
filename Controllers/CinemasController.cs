cd9884a003b6e9abfd67e03fcb6f6827e53d7bc6
mohammedatefa <mohamed.atef.abdelsbor.com>
2023-12-14 15:24:37 +0200

    add actions of update ,creaet and delete action to the cinema controloer

--- Controllers/CinemasController.cs
+++ Controllers/CinemasController.cs
@@ -7,7 +7,7 @@ namespace e_Tickets.Controllers
     public class CinemasController : Controller
     {
         private readonly IGenericRepository<Cinema> context;
-
+            
         public CinemasController(IGenericRepository<Cinema> _context)
         {
             context = _context;
@@ -17,5 +17,65 @@ namespace e_Tickets.Controllers
             var cinemas = await context.Getall();
             return View(cinemas);
         }
+        #region Create New Cinema Action
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new Cinema());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Cinema cinema)
+        {
+            if (ModelState.IsValid)
+            {
+                await context.Add(cinema);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(cinema);
+            }
+        }
+        #endregion
+
+        #region Update Current Cinema Action
+        [HttpGet]
+        public async Task<IActionResult> Update(int id)
+        {
+            var found = await context.GetById(id);
+            if (found is not null)
+            {
+                return View(found);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(int id, Cinema cinema)
+        {
+            if (ModelState.IsValid)
+            {
+                await context.Update(id, cinema);
+                return RedirectToAction("index");
+            }
+            else
+            {
+                return View(cinema);
+            }
+        }
+        #endregion
+
+        #region Delate Cinema Action
+        public async Task<IActionResult> Delete(int id)
+        {
+            var found = await context.GetById(id);
+            if (found != null)
+            {
+                await context.Delete(id);
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        } 
+        #endregion
     }
 }
--- Models/Cinema.cs
+++ Models/Cinema.cs
@@ -6,14 +6,16 @@ namespace e_Tickets.Models
     public class Cinema:Basemodel
     {
         
-        [Required]
+        [Required(ErrorMessage ="the name of cinema is required ")]
         [DisplayName("Name")]
         public string? Name { get; set; }
-        [Required]
+        [Required(ErrorMessage ="The Logo Url Is Required")]
         [DisplayName("logo")]
+        [Url(ErrorMessage ="You Should Use Valid Url..")]
         public string? Logo { get; set; }
-        [Required]
+        [Required(ErrorMessage ="the description of the cenima is required ")]
         [DisplayName("Description")]
+        [StringLength(500,MinimumLength =10,ErrorMessage ="the description must be more than 10 and less than 500 ")]
         public string? Description { get; set; }
 
 
